d55bf3fb727b793dd0df5d992865ff1e6790afb0
Glemakk <kovgar.brand@gmail.com>
2021-07-17 21:41:54 +0200

    Remade Modal on hooks with useEffect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,71 @@
-import React, { Component } from 'react'
+import { useEffect } from 'react'
 import { Overlay, ModalWrapper } from './Modal.styled'
 
-//===========
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onImagesClick)
-  }
+const Modal = ({ onClose, onOpen }) => {
+  useEffect(() => {
+    // console.log('запускаем')
+    window.addEventListener('keydown', onImagesClick)
 
-  onImagesClick = (e) => {
+    return () => {
+      // console.log('чистим за собой')
+      window.addEventListener('keydown', onImagesClick)
+    }
+  })
+
+  const onImagesClick = (e) => {
     if (e.code === 'Escape') {
-      this.props.onClose()
+      onClose()
     }
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onImagesClick)
-  }
-
-  handleBackdropClick = (e) => {
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onClose()
+      onClose()
     }
   }
-  render() {
-    return (
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalWrapper>
-          <img
-            src={this.props.onOpen.largeImageURL}
-            alt={this.props.onOpen.tags}
-          />
-        </ModalWrapper>
-      </Overlay>
-    )
-  }
+
+  return (
+    <Overlay onClick={handleBackdropClick}>
+      <ModalWrapper>
+        <img src={onOpen.largeImageURL} alt={onOpen.tags} />
+      </ModalWrapper>
+    </Overlay>
+  )
 }
+
+export default Modal
+
+//===========the old class
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.onImagesClick)
+//   }
+
+//   onImagesClick = (e) => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose()
+//     }
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.onImagesClick)
+//   }
+
+//   handleBackdropClick = (e) => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose()
+//     }
+//   }
+//   render() {
+//     return (
+//       <Overlay onClick={this.handleBackdropClick}>
+//         <ModalWrapper>
+//           <img
+//             src={this.props.onOpen.largeImageURL}
+//             alt={this.props.onOpen.tags}
+//           />
+//         </ModalWrapper>
+//       </Overlay>
+//     )
+//   }
+// }
